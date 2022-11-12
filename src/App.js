e661e3ec7fea391ec302cce5727b3d77f85d3d9c
Freddy Alexander Morales <freddy.morales@mercadolibre.com.co>
2022-11-12 00:46:32 -0500

    fix extra content

--- src/App.js
+++ src/App.js
@@ -12,14 +12,14 @@ function App() {
 
   const { theme } = useContext(ThemeContext);
 
-  console.log("%cDEVELOPER PORTFOLIO", `color:${theme.primary}; font-size:50px`);
-  console.log("%chttps://github.com/hhhrrrttt222111/developer-portfolio", `color:${theme.tertiary}; font-size:20px`);
+  console.log("%cPORTFOLIO CARLS MUSCLE", `color:${theme.primary}; font-size:50px`);
+  console.log("%chttps://github.com/br2bb7t/portfolio-carls-muscle", `color:${theme.tertiary}; font-size:20px`);
   // console.log = console.warn = console.error = () => {};
 
   return (
     <div className="app">
       <Router>
-        <ScrollToTop/>
+        <ScrollToTop />
         <Switch>
           <Route path="/" exact component={Main} />
           <Route path="/blog" exact component={BlogPage} />
